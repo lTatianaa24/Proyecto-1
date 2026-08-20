019997ecc3542be17cd0ac70f3e17c8cbfe16179
lTatianaa24 <leidytatianavalenzuela11@gmail.com>
2026-08-19 19:41:11 -0500

    Subiendo mis archivitoss

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception { //Esto es un atributo de un método que hace parte de la clase App//
+        System.out.println("Hello, World!"); /*El main siempre se utiliza para ejecutar el código*/
+    }
+}
---
+++ src/MiClase.java
@@ -0,0 +1,14 @@
+public class MiClase {
+    private int edad = 18; 
+    private double altura = 175;
+    private char sexo = 'f';
+    private boolean aprobado = true;
+    String nombre = "Tatiana Valenzuela";
+    String saludo = "Hola, " + nombre;
+    private double precio = 9.99;
+    private int precioCortado = (int) precio; /* Casteo: Conversión entre tipos */
+    private int entero = 5;
+    private double decimal = entero; /*Forma de presentarlo cuando es caso contrario, se ajusta de manera automático*/
+
+    
+}
